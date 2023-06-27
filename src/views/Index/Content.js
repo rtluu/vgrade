72a51bb239a50bbfa915cd0556c048b80ef9210d
Ryan Luu <ryanluu@Ryans-MBP.attlocal.net>
2023-06-26 20:44:25 -0700

    Body content

--- src/views/Index/Content.js
+++ src/views/Index/Content.js
@@ -3,8 +3,14 @@ import React from "react";
 function Content() {
     return (
         <div>
-            <div className="content">'
-                <div id=""><p>make a living from your virtual worlds</p></div>
+            <div className="content">
+                <div className="content-left-colummn">
+                    <p>make a living from your virtual worlds</p>
+                </div>
+                <div className="content-bottom-column">
+                    <a className="link" href="mailto:osebosebo@gmail.com"><p>collaborate with us</p></a>
+                    <p className="user-group">for people who want to monetize their games in Roblox</p>
+                </div>
             </div>
         </div>
     );
