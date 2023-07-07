0ad324d3a3c10e3ec3904dec2039e6fb9fd59327
Ryan Luu <ryanluu@Ryans-MBP.attlocal.net>
2023-07-06 20:15:24 -0700

    nvmrc file

---
+++ build/precache-manifest.5ba63c64fee780478cf22ef678bedaa3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b94e08ac7ddcb73da5e68383bab2ba07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d269c617a18dd92218b",
+    "url": "/static/css/main.3cae16a4.chunk.css"
+  },
+  {
+    "revision": "7a554263377a5320f647",
+    "url": "/static/js/2.f8b3151b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.f8b3151b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d269c617a18dd92218b",
+    "url": "/static/js/main.0c305389.chunk.js"
+  },
+  {
+    "revision": "b256f3c5e116916af76a",
+    "url": "/static/js/runtime-main.e697eb94.js"
+  },
+  {
+    "revision": "9632144b89660f0f032a3a0a01e533c7",
+    "url": "/static/media/HelveticaNowDisplay-Regular.9632144b.ttf"
+  }
+]);
